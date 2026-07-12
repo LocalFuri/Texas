9c5d4d96d7b3adbd571d0084bcf205c5a04b465d
Marcus Boehm <m.adolph@gmx.net>
2026-07-12 17:05:30 +0200

    2026-07-12 v 9

--- Assets/Scripts/Audio/TableSoundManager.cs
+++ Assets/Scripts/Audio/TableSoundManager.cs
@@ -76,6 +76,10 @@ namespace TexasHoldem
 
             switch (action)
             {
+                case BettingAction.Check:
+                    PlayKnockKnock();
+                    break;
+
                 case BettingAction.Fold:
                     PlayFold();
                     break;
